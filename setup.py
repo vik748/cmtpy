94daab4ba67698cfc35b3559019a6a629e16bfdf
vik748 <vikrantshah@gmail.com>
2020-05-21 21:14:49 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="cmtpy",
+    packages=["cmtpy"],
+    version="0.0.1",
+    description="Tools for manipulating image contrast",
+    author="vs",
+    author_email="vikrantshah+py@gmail.com",
+    license='MIT',
+    url="https://github.com/vik748/cmtpy",
+    keywords=["contrast", "histogram", "enhancement"],
+    classifiers=[],
+    install_requires=[
+        'scipy'
+    ],
+)
